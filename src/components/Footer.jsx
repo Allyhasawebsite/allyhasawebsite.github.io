26a329d2811b1dc40431a49815431f78380f13f1
Aronn Laurel <laurelallyanna@gmail.com>
2026-02-22 03:08:10 -0800

    Added Infinite scroll banner

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
+import React from 'react';
+
+const headerContent = [
+  { message: "welcome to ally's motion media website where you can find some fun stuff :)" },
+  { message: "this website was made by Allyanna's brother - Aronn"}
+]
 
 const Footer = () => {
   
   return (
-      <div>
+      <div className="overflow-hidden bg-black flex">
+
+      <ul className="flex gap-10 bg-black text-white py-4 animate-infinite-scroll">
+        {[...headerContent, ...headerContent].map((item) => {
+
+          return (
+          <li className="flex gap-2">
+            <p className="text-gray-100"> {item.message} </p>
+          </li>
+        )})}
+      </ul>
 
       </div>
   );
